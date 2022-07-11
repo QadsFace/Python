7a5d07709de75484ea06ee21b7869bb52cb3e9bd
QadsFace <108893686+QadsFace@users.noreply.github.com>
2022-07-11 10:09:06 -0400

    c

---
+++ Helloworld.py
@@ -0,0 +1,9 @@
+msg="My name is Walter Hartwell White. I live at 308 Negra Arroyo Lane Albuquerque New Mexico 87104."
+x=2
+y=5
+z=2.3
+my_boolean=True
+print(msg)
+print(type(msg))
+print(2+4)
+my_boolean=False
